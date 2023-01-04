e51942f6b78a5b681b96430d78989a6a335f6c8b
우성일 <wooniverse7@gmail.com>
2023-01-05 06:40:12 +0900

    콜렉션 연습

---
+++ src/Collection/Test/Main.java
@@ -0,0 +1,18 @@
+package Collection.Test;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(5);
+
+        for (int i = 0; i < 10; i += 2) {
+            al.add(i);
+        }
+        System.out.println(al.get(3));
+        al.isEmpty();
+        al.size();
+        al.set(1, 5);
+        System.out.println(al.get(1));
+    }
+}
