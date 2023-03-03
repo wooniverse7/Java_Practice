fa5ad95b2050bb3e67efa1fbb3c48f645b12d9e4
우성일 <wooniverse7@gmail.com>
2023-03-03 11:26:34 +0900

    Daliy Coding P44 solved

--- src/Section2_Daily/OneNote/Solution.java
+++ src/Section2_Daily/OneNote/Solution.java
@@ -1,143 +1,55 @@
 package Section2_Daily.OneNote;
 
-import java.util.Arrays;
-
 public class Solution {
     public static void main(String[] args) {
-        Character[][] matrix = new Character[][]{
-                {'A', 'B', 'C'},
-                {'D', 'E', 'F'},
-                {'G', 'H', 'I'},
-        };
-        String output = spiralTraversal(matrix);
-        System.out.println(output); // --> "ABCFIHGDE"
+        int total = 10;
+        int[] coins = new int[]{1, 5};
+        int output = coinChange(total, coins);
+        System.out.println(output); // --> 3
 
-        matrix = new Character[][]{
-                {'T', 'y', 'r', 'i'},
-                {'i', 's', 't', 'o'},
-                {'n', 'r', 'e', 'n'},
-                {'n', 'a', 'L', ' '},
-        };
-        output = spiralTraversal(matrix);
-        System.out.println(output); // --> 'Tyrion Lannister'
+        total = 10;
+        coins = new int[]{1, 2, 5};
+        output = coinChange(total, coins);
+        System.out.println(output); // --> 10
+
+        total = 4;
+        coins = new int[]{1, 2, 3};
+        output = coinChange(total, coins);
+        System.out.println(output); // --> 4 ([1, 1, 1, 1], [1, 1, 2], [2, 2], [1, 3])
     }
 
+    public static int coinChange(int total, int[] coins) {
+        // TODO :
+        // DP
+        /**
+         *   dp[i]는 i원까지의 동전의 경우의 수
+         *   dp[i] += dp[i-coin] 식은 j원이 되기 위해서 추가되는 동전의 수
+         *
+         *   예를 들어 j가 10이고 동전 1,2,5를 갖고 있을 때의 경우의 수
+         *   dp[9] + 1 원
+         *   dp[8] + 2 원
+         *   dp[5] + 5 원
+         *
+         *  dp[9], d[8], dp[5] 또한 각각 해당 인덱스까지의 모든 경우의 수 이기 때문에 전부 더해주면
+         *  dp[10] 의 총 경우의 수
+         */
+        int[] dp = new int[total + 1];
 
-    public static String spiralTraversal(Character[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        // Defining the boundaries of the matrix.
-        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+        dp[0] = 1;
 
-        // Defining the direction in which the array is to be traversed.
-        int dir = 1;
-        StringBuilder sb = new StringBuilder();
-
-        while (top <= bottom && left <= right) {
-
-            if (dir == 1) {    // moving left->right
-                for (int i = left; i <= right; ++i) {
-                    sb.append(matrix[top][i]);
-//                    System.out.print(matrix[top][i] + " ");
-                }
-                // Since we have traversed the whole first
-                // row, move down to the next row.
-                ++top;
-                dir = 2;
-            } else if (dir == 2) {     // moving top->bottom
-                for (int i = top; i <= bottom; ++i) {
-                    sb.append(matrix[i][right]);
-//                    System.out.print(matrix[i][right] + " ");
-                }
-                // Since we have traversed the whole last
-                // column, move left to the previous column.
-                --right;
-                dir = 3;
-            } else if (dir == 3) {     // moving right->left
-                for (int i = right; i >= left; --i) {
-                    sb.append(matrix[bottom][i]);
-//                    System.out.print(matrix[bottom][i] + " ");
-                }
-                // Since we have traversed the whole last
-                // row, move up to the previous row.
-                --bottom;
-                dir = 4;
-            } else if (dir == 4) {     // moving bottom->up
-                for (int i = bottom; i >= top; --i) {
-                    sb.append(matrix[i][left]);
-//                    System.out.print(matrix[i][left] + " ");
-                }
-                // Since we have traversed the whole first
-                // col, move right to the next column.
-                ++left;
-                dir = 1;
+        for(int coin : coins){
+            // {1, 2, 5}, 10
+            for (int i = coin; i <= total; i++) {
+                dp[i] += dp[i-coin];
+                // coin : 1
+                // dp[] : , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                // coin : 2
+                // dp[] : , 1, 2, 2, 3, 3, 4, 4, 5, 5, 6
+                // coin : 5
+                // dp[] : , 1, 2, 2, 3, 4, 5, 6, 7, 8, 10
             }
         }
 
-        String rst = sb.toString();
-        return rst;
-    }
-
-
-    public static int getItemFromTwoSortedArrays(int[] arr1, int[] arr2, int k) {
-        int leftIdx = 0;
-        int rightIdx = 0;
-        while (k > 0) {
-            // 이진 탐색을 위해 각 배열에서 k를 절반으로 쪼개서 카운트 한다.
-            int cnt = (int)Math.ceil(((double) k) / 2);
-            int leftStep = cnt;
-            int rightStep = cnt;
-
-            // 엣지 케이스
-            // 카운트가 남았음에도 배열의 끝에 도달하면 k를 나머지 배열쪽으로 넘긴다.
-            if (leftIdx == arr1.length) {
-                rightIdx = rightIdx + k;
-                break;
-            }
-
-            if (rightIdx == arr2.length) {
-                leftIdx = leftIdx + k;
-                break;
-            }
-
-            // 엣지 케이스
-            // 현재 카운트가 남아있는 후보 요소들보다 많을 경우, leftStep(현재 할당량)을 남아있는 요소들의 개수로 바꾼다.
-            if (cnt > arr1.length - leftIdx) leftStep = arr1.length - leftIdx;
-            if (cnt > arr2.length - rightIdx) rightStep = arr2.length - rightIdx;
-
-            // 두 배열의 현재 검사 요소 위치를 비교해서, 그 값이 작은 배열은 비교한 위치 앞에 있는 요소들을 모두 후보군에서 제외시킨다.
-            if (arr1[leftIdx + leftStep - 1] < arr2[rightIdx + rightStep - 1]) {
-                leftIdx = leftIdx + leftStep;
-                // 처리가 끝나면 k값을 절반으로 떨어뜨린다.
-                k = k - leftStep;
-            } else {
-                rightIdx = rightIdx + rightStep;
-                k = k - rightStep;
-            }
-        }
-        int leftMax = (leftIdx - 1 < arr1.length) ? arr1[leftIdx - 1] : -1;
-        int rightMax = (rightIdx - 1 < arr2.length) ? arr2[rightIdx - 1] : -1;
-
-        return Math.max(leftMax, rightMax);
-    }
-
-    public static int binarySearch(int[] arr, int target) {
-        // TODO:
-        int lf = 0;
-        int rt = arr.length - 1;
-
-        while(lf <= rt){
-            int mid = (lf + rt) / 2;
-
-            if(arr[mid] == target){
-                return mid;
-            }else if(arr[mid] > target){
-                rt = mid - 1;
-            }else{
-                lf = mid + 1;
-            }
-        }
-
-        return -1;
+        return dp[total];
     }
 }
